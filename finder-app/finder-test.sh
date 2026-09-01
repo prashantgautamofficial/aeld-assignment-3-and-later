95c98f5818edcc64e1868d4b4e004a816fe2b321
prashantgautamofficial <prashantgautamofficial@gmail.com>
2026-09-01 11:23:46 +0530

    Assignment 3 Part 2: Manual Kernel and Root Filesystem Build Successfull : Fix finder-test.sh: call ./writer instead of bare writer (not on target PATH)

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,7 +49,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
